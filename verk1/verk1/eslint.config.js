69a4794e3a57b220169da2e5e19adbea6a04894d
soleynott <soleynott@gmail.com>
2025-11-25 13:48:32 +0000

    integrate Prettier with ESlint

--- verk1/verk1/eslint.config.js
+++ verk1/verk1/eslint.config.js
@@ -1,9 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const esLintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
 
 module.exports = defineConfig([
   expoConfig,
+  esLintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
   },
