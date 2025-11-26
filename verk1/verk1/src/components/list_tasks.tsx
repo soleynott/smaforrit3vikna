262099a7130ce3fb3d7d06ceae28bc5e71a3ca79
soleynott <soleynott@gmail.com>
2025-11-26 16:49:32 +0000

    update a gitignore og bara import i list.tsx og list_tasks.tsx

--- verk1/verk1/src/components/list_tasks.tsx
+++ verk1/verk1/src/components/list_tasks.tsx
@@ -1,1 +1,5 @@
 /*in list view, each task in one list*/
+import { View } from "react-native";
+import data from "../../resources/data.json";
+import { FlatList } from "react-native-gesture-handler";
+import { ListItem } from "./lists_item";
---
+++ verk1/verk1/src/views/list_view/list.tsx
@@ -0,0 +1,3 @@
+import { useLocalSearchParams, useRouter  } from "expo-router"
+import { Image, Text, TouchableOpacity, View} from "react-native"
+import { BoardList } from "@/src/components/board_list/board_lists"
