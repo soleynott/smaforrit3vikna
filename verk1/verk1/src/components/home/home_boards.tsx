21b7abcdf182bbe5d74f976b187f8cf2f5c8176f
Egillisfeld <egillisfeld2908@gmail.com>
2025-11-26 16:26:09 +0000

    home and board linked

--- verk1/verk1/src/components/home/home_boards.tsx
+++ verk1/verk1/src/components/home/home_boards.tsx
@@ -1,27 +1,43 @@
 
-import { View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import data from "../../resources/data.json";
 import { FlatList } from "react-native-gesture-handler";
 import { BoardThumbnail } from "../../types/board_thumbnail";
 import { BoardItem } from "./board_item";
+import  { useRouter } from "expo-router"
 
 export function HomeBoards() {
     const images = data.boards as unknown as BoardThumbnail[];
+ const router = useRouter();
     return (
         <View style={{flex:1}}>
-            <FlatList<BoardThumbnail>
-                numColumns={1}
-                data={images}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}) => <BoardItem
+             <FlatList<BoardThumbnail>
+            numColumns={1}
+            data={images}
+            renderItem={({ item }) => (
+                <TouchableOpacity
+                onPress={() =>
+                    router.push({
+                    pathname: '/board',
+                    params: { id: item.id.toString() }, 
+                    })
+                }
+                accessibilityLabel={`Go to board ${item.name}`}
+                accessibilityRole="button"
+                >
+                <BoardItem
                     id={item.id}
                     name={item.name}
                     description={item.description}
-                    image={item.thumbnailPhoto}/>}
+                    image={item.thumbnailPhoto}
+                />
+                </TouchableOpacity>
+            )}
             />
-        </View>
-    )
-}
+
+                    </View>
+                )
+            }
 
 
 
