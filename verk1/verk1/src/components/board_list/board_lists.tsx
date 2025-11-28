bc59de4dd5cf491846a3a24497fe28518e786840
Egillisfeld <egillisfeld2908@gmail.com>
2025-11-28 14:13:35 +0000

    fixed display issues in lists

--- verk1/verk1/src/components/board_list/board_lists.tsx
+++ verk1/verk1/src/components/board_list/board_lists.tsx
@@ -7,19 +7,16 @@ import { ListItem } from "./lists_item";
 import  { useRouter } from "expo-router"
 
 type BoardListProps = {
-  boardId: number; 
+  lists: ListsThumbnail[]; 
 };
-export function BoardList({ boardId }: BoardListProps) {
-  const lists = data.lists as unknown as ListsThumbnail[];
-  //filter lists by what board they belong to
-  const filteredLists = lists.filter(list => list.boardId === boardId);
+export function BoardList({ lists }: BoardListProps) {
   const router = useRouter();
 
   return (
     <View style={{ flex: 1 }}>
       <FlatList<ListsThumbnail>
         numColumns={1}
-        data={filteredLists}
+        data={lists}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <TouchableOpacity
