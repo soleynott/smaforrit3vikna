2d723c3c93419b1e8202ece466f5489262a6f8c2
mattthildur <matthildursverris@gmail.com>
2025-12-01 16:34:09 +0000

    main

--- verk2/contactor/app/_layout.tsx
+++ verk2/contactor/app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+  <GestureHandlerRootView>
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Contact",
+        }}
+      />
+    </Stack>
+  </GestureHandlerRootView>
+  );
 }
