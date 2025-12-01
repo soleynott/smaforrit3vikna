b0a4a12a314bb732687cf7eb335cfbd00bce1ad0
soleynott <soleynott@gmail.com>
2025-12-01 14:27:18 +0000

    notes

---
+++ verk2/contactor/src/views/contact/contact.tsx
@@ -0,0 +1,12 @@
+/**the contact detail screen
+ * 
+ * should include:
+ *      contact photo
+ *      name
+ *      phone number
+ *      option to edit contact
+ * 
+ * back arrow leads back to home screen 
+ *
+ * 
+ */
---
+++ verk2/contactor/src/views/main/main.tsx
@@ -0,0 +1,10 @@
+/** the main screen
+ * should include:
+ *      search bar
+ *      list of contacts
+ *      option to add new contact
+ * 
+ * when a contact's name is clicked it should
+ * route to that contact's detail view
+ * 
+ */
