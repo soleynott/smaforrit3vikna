20145a2c175eeea7ef50b639457a0137284759bf
soleynott <soleynott@gmail.com>
2025-12-01 15:38:00 +0000

    app adding contact.tsx

--- verk2/contactor/app/_layout.tsx
+++ verk2/contactor/app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Contacts",
+            }}
+          />
+          <Stack.Screen
+            name="contact"
+            options={{
+              title: "contact details",
+            }}
+          />
+        </Stack>
+    </GestureHandlerRootView>
+  );
 }
