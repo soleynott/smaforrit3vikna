a14ddf58e2b83392af95c087cd5b3a4acae0cc35
soleynott <soleynott@gmail.com>
2025-12-09 13:06:27 +0000

    updated _layout

--- verk3/drCinema/app/_layout.tsx
+++ verk3/drCinema/app/_layout.tsx
@@ -1,5 +1,47 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
-  return <Stack />;
+	return (
+		<GestureHandlerRootView>
+			<Stack>
+				<Stack.Screen
+					name="homescreen"
+					options={{
+						title: 'Dr. Cinema',
+					}}
+				/>
+				<Stack.Screen
+					name="cinemas"
+					options={{
+						title: 'Cinemas',
+					}}
+				/>
+				<Stack.Screen
+					name="cinemaDetail"
+					options={{
+						title: 'Cinema detail',
+					}}
+				/>
+				<Stack.Screen
+					name="movie"
+					options={{
+						title: 'Movie',
+					}}
+				/>
+				<Stack.Screen
+					name="upcoming"
+					options={{
+						title: 'Upcoming movies',
+					}}
+				/>
+				<Stack.Screen
+					name="favourites"
+					options={{
+						title: 'Your Favourites',
+					}}
+				/>
+			</Stack>
+		</GestureHandlerRootView>
+	);
 }
