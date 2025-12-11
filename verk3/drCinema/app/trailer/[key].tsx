908343e527b4bd00f94998cde5c6f668541d230a
soleynott <soleynott@gmail.com>
2025-12-11 10:26:30 +0000

    changed from embedded yt player in app to opening externally since it wont work in expogo

--- verk3/drCinema/app/trailer/[key].tsx
+++ verk3/drCinema/app/trailer/[key].tsx
@@ -1,26 +1,24 @@
-import { View, ActivityIndicator, Text } from 'react-native';
-import { WebView } from 'react-native-webview';
-import { useLocalSearchParams } from 'expo-router';
+import { useEffect } from 'react';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Linking, Text, View } from 'react-native';
 
 export default function TrailerScreen() {
 	const { key } = useLocalSearchParams();
-	if (!key) {
-		return <Text>No trailer available</Text>;
-	}
+	const router = useRouter();
 
-	const youtubeUrl = `https://www.youtube-nocookie.com/embed/${key}`;
+	useEffect(() => {
+		if (!key) return;
+
+		const url = `https://www.youtube.com/watch?v=${key}`;
+		Linking.openURL(url);
+
+		// Automatically go back after opening browser
+		router.back();
+	}, [key]);
 
 	return (
-		<View style={{ flex: 1 }}>
-			<WebView
-				source={{ uri: youtubeUrl }}
-				style={{ flex: 1 }}
-				allowsFullscreenVideo
-				javaScriptEnabled
-				domStorageEnabled
-				renderLoading={() => <ActivityIndicator style={{ flex: 1 }} />}
-				startInLoadingState
-			/>
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Opening trailer...</Text>
 		</View>
 	);
 }
